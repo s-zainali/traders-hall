3e1e82072149de5258a61b8c942fbd6fc9f70eb6
Syed Zain Ali <szainali284@gmail.com>
2026-07-20 20:22:31 +0500

    added tailwind

--- traders-hall-frontend/src/App.vue
+++ traders-hall-frontend/src/App.vue
@@ -1,11 +1,10 @@
-<script setup></script>
+<script setup>
+import Card from './Components/Card.vue';
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Card />
 </template>
 
 <style scoped></style>
