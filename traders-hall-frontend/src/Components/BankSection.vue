612db9351098ee0c6c5ea09cf689eb7593e5c437
Syed Zain Ali <szainali284@gmail.com>
2026-07-21 15:30:59 +0500

    added card deck container

--- traders-hall-frontend/src/Components/BankSection.vue
+++ traders-hall-frontend/src/Components/BankSection.vue
@@ -4,6 +4,7 @@ import PointCard from './Cards/Currency/PointCard.vue';
 import BankerCard from './Cards/BankerCard.vue';
 import { ref } from 'vue';
 import BankerModal from './Modals/BankerModal.vue';
+import CardDeck from './CardDeck.vue';
 
 const activeModal = ref('null')
 
@@ -14,7 +15,9 @@ const activeModal = ref('null')
     <div class="overflow-hidden relative p-6 min-w-lg border-2 border-gray-light rounded-[1rem]">
         <h1 class="text-3xl text-gray-2x-light font-bold pb-6"> Bank </h1>
         <div class="flex justify-between">
-            <PointCard />
+            <CardDeck>
+                <PointCard v-for="value in 2" />
+            </CardDeck>
             <BankerCard @activate-modal="activeModal = $event"/>
         </div>
         <BankerModal v-if="activeModal === 'bankerModal'" @close-modal="activeModal = null"/>
