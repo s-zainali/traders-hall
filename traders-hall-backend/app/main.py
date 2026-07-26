bec715962cfb05d7cd04504a69f26498a9033f0b
Syed Zain Ali <szainali284@gmail.com>
2026-07-26 22:22:19 +0500

    removed redundant print

--- traders-hall-backend/app/main.py
+++ traders-hall-backend/app/main.py
@@ -22,7 +22,6 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title=settings.PROJECT_NAME, version="0.1.0", lifespan=lifespan)
 
 frontend_dir = "dist" 
-print(os.path.exists(frontend_dir), "HELLOOOOO")
 
 if os.path.exists(frontend_dir):
     app.frontend("/", directory=frontend_dir, fallback="index.html")
